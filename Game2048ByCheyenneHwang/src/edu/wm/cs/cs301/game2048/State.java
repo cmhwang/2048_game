f19078131fe2c843fb41bc142457f4ab61c8499f
Cheyenne Hwang <cmhwang@email.wm.edu>
2022-09-16 14:31:11 -0400

    implement get and setValue() methods
    warning: still an issue with double slide
    description: actually utilized getValue() and setValue() methods throughout code

--- Game2048ByCheyenneHwang/src/edu/wm/cs/cs301/game2048/State.java
+++ Game2048ByCheyenneHwang/src/edu/wm/cs/cs301/game2048/State.java
@@ -2,7 +2,7 @@ package edu.wm.cs.cs301.game2048;
 
 public class State implements GameState {
 	
-	int[][] board;// TODO: need to actually work this into game
+	int[][] board;//CONSIDER making this private
 	int myInt;
 	double myDouble;
 	
@@ -34,54 +34,46 @@ public class State implements GameState {
 	}
 	
 	public State(GameState original) {
-		// TODO Auto-generated constructor stub
+		// TODO 
 		board = new int[4][4];
-		for (int i=0; i<4; i++) {
-			for (int j=0; j<4; j++) {
-				board[i][j] = 0;
-			}
-		}
+		setEmptyBoard();
 	}
 
 	
 	public State() {
-		// TODO Auto-generated constructor stub
+		// TODO 
 		board = new int[4][4];
-		for (int i=0; i<4; i++) {
-			for (int j=0; j<4; j++) {
-				board[i][j] = 0;
-			}
-		}
+		setEmptyBoard();
 	}
 
 
 	@Override
 	public int getValue(int xCoordinate, int yCoordinate) {
-		// TODO Auto-generated method stub
+		// TODO 
 		int value = board[yCoordinate][xCoordinate];
 		return value;
 	}
 
 	@Override
 	public void setValue(int xCoordinate, int yCoordinate, int value) {
-		// TODO Auto-generated method stub - consider if statement to check if value param is appropriate
+		// TODO consider if statement to check if value param is appropriate
 		board[yCoordinate][xCoordinate] = value;
 
 	}
 
 	@Override
 	public void setEmptyBoard() {
-		// TODO Auto-generated method stub
+		// TODO 
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
-				board[i][j] = 0;
+				setValue(i, j, 0);
 			}
 		}
 	}
 
 	@Override
 	public boolean addTile() {
-		// TODO Auto-generated method stub - CONSIDER changing to list if allowed to import other packages
+		// TODO 
 		
 		int counter = 0;
 		int[] rowVal = new int[16];
@@ -89,7 +81,7 @@ public class State implements GameState {
 		
 		for (int k = 0; k < 4; k++) {// creates array of tile coordinates that are 0 to choose from randomly later
 			for (int l = 0; l < 4; l++)
-			if (board[k][l] == 0) {
+			if (getValue(l, k) == 0) {
 				rowVal[counter] = k;
 				colVal[counter] = l;
 				counter = counter + 1;
@@ -101,19 +93,16 @@ public class State implements GameState {
 			int min = 0;
 			int max1 = counter - 1;
 			
-			int randomTileVal = (int)Math.floor(Math.random()*(max1-min+1)+min);
+			int randomTileVal = (int)Math.floor(Math.random()*(max1+1));
 			int randomTileRow = rowVal[randomTileVal];
 			int randomTileCol = colVal[randomTileVal];
-			
-			int max2 = 1;
-			
-			int randomVal = (int)Math.floor(Math.random()*(max2-min+1)+min); // creates random value to randomly choose if new tile 4 or 2
+				
+			int randomVal = (int)Math.floor(Math.random()*(2)); // creates random value to randomly choose if new tile 4 or 2
 			
 			if (randomVal == 0) {
-				// TODO
-				board[randomTileRow][randomTileCol] = 2;// CONISDER - finding way to use setValue
+				setValue(randomTileCol, randomTileRow, 2);
 			}else {
-				board[randomTileRow][randomTileCol] = 4;
+				setValue(randomTileCol, randomTileRow, 4);
 			}
 			return true;
 		}
@@ -121,10 +110,10 @@ public class State implements GameState {
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
+		// TODO 
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
-				if (board[i][j] != 0) {
+				if (getValue(i, j) != 0) {
 					return false;
 				}
 			}
@@ -134,77 +123,77 @@ public class State implements GameState {
 
 	@Override
 	public boolean canMerge() {
-		// TODO Auto-generated method stub
+		// TODO consider more efficient way?
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
 				if ((i > 0) && (i < 3) && (j > 0) && (j < 3)) { // tiles in the middle
-					if (board[i][j] == board[i-1][j]) { // check with upper tile
+					if (getValue(j , i) == getValue(i-1, j)) { // check with upper tile
 						return true;
-					}else if (board[i][j] == board[i+1][j]) {//check with bottom tile
+					}else if (getValue(j , i) == getValue(j, i+1)) {//check with bottom tile
 						return true;
-					}else if (board[i][j] == board[i][j+1]) {//check right tile
+					}else if (getValue(j , i) == getValue(j+1, i)) {//check right tile
 						return true;
-					}else if (board[i][j] == board[i][j-1]) {//check left tile
+					}else if (getValue(j , i) == getValue(j-1, i)) {//check left tile
 						return true;
 					}
 				} else if (i == 0) {//tiles in the top row
 					if (j == 0) {//top left corner tile case
-						if (board[i][j] == board[i+1][j]) {//check with bottom tile
+						if (getValue(j , i) == getValue(j, i+1)) {//check with bottom tile
 							return true;
-						}else if (board[i][j] == board[i][j+1]) {//check right tile
+						}else if (getValue(j , i) == getValue(j+1, i)) {//check right tile
 							return true;
 						}
 					}else if (j ==3) {//top right corner tile case
-						if (board[i][j] == board[i+1][j]) {//check with bottom tile
+						if (getValue(j , i) == getValue(j, i+1)) {//check with bottom tile
 							return true;
-						}else if (board[i][j] == board[i][j-1]) {//check left tile
+						}else if (getValue(j , i) == getValue(j-1, i)) {//check left tile
 							return true;
 						}
 					}else {//other two middle tiles in the row
-						if (board[i][j] == board[i+1][j]) {//check with bottom tile
+						if (getValue(j, i) == getValue(j, i+1)) {//check with bottom tile
 							return true;
-						}else if (board[i][j] == board[i][j+1]) {//check right tile
+						}else if (getValue(j , i) == getValue(j+1, i)) {//check right tile
 							return true;
-						}else if (board[i][j] == board[i][j-1]) {//check left tile
+						}else if (getValue(j , i) == getValue(j-1, i)) {//check left tile
 							return true;
 						}
 					}
 				}else if (i == 3) {//tiles in the bottom row
 					if (j == 0) {//bottom left corner tile case
-						if (board[i][j] == board[i-1][j]) { // check with upper tile
+						if (getValue(j , i) == getValue(j, i-1)) { // check with upper tile
 							return true;
-						}else if (board[i][j] == board[i][j+1]) {//check right tile
+						}else if (getValue(j , i) == getValue(j+1, i)) {//check right tile
 							return true;
 						}
 					}else if (j ==3) {//bottom right corner tile case
-						if (board[i][j] == board[i-1][j]) { // check with upper tile
+						if (getValue(j , i) == getValue(j, i-1)) { // check with upper tile
 							return true;
-						}else if (board[i][j] == board[i][j-1]) {//check left tile
+						}else if (getValue(j , i) == getValue(j-1, i)) {//check left tile
 							return true;
 						}
 					}else{//other two middle tiles in the row
-						if (board[i][j] == board[i-1][j]) { // check with upper tile
+						if (getValue(j , i) == getValue(j, i-1)) { // check with upper tile
 							return true;
-						}else if (board[i][j] == board[i][j+1]) {//check right tile
+						}else if (getValue(j , i) == getValue(j+1, i)) {//check right tile
 							return true;
-						}else if (board[i][j] == board[i][j-1]) {//check left tile
+						}else if (getValue(j , i) == getValue(j-1, i)) {//check left tile
 							return true;
 						}
 					}
 				}else if (j == 0) {// left most column minus corners case
-					if (board[i][j] == board[i-1][j]) { // check with upper tile
+					if (getValue(j , i) == getValue(j, i-1)) { // check with upper tile
 						return true;
-					}else if (board[i][j] == board[i+1][j]) {//check with bottom tile
+					}else if (getValue(j , i) == getValue(j, i+1)) {//check with bottom tile
 						return true;
-					}else if (board[i][j] == board[i][j+1]) {//check right tile
+					}else if (getValue(j , i) == getValue(j+1, i)) {//check right tile
 						return true;
 					}
 				}else if (j == 3) {//right most column minus corners case
-					if (board[i][j] == board[i-1][j]) { // check with upper tile
+					if (getValue(j , i) == getValue(j, i-1)) { // check with upper tile
 						return true;
-					}else if (board[i][j] == board[i+1][j]) {//check with bottom tile
+					}else if (getValue(j , i) == getValue(j, i+1)) {//check with bottom tile
 						return true;
-					}else if (board[i][j] == board[i][j-1]) {//check left tile
+					}else if (getValue(j , i) == getValue(j-1, i)) {//check left tile
 						return true;
 					}
 				}
@@ -216,16 +205,17 @@ public class State implements GameState {
 
 	@Override
 	public boolean reachedThreshold() {
-		// TODO Auto-generated method stub
-		int cur_max = 0;
+		// TODO 
+		int curMax = 0;
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
-				if (board[i][j] > cur_max) {
-					cur_max = board[i][j];
+				int curVal = getValue(i, j);
+				if (curVal > curMax) {
+					curMax = curVal;
 				}
 			}
 		}
-		if (cur_max >= 2048) {
+		if (curMax >= 2048) {
 			return true;
 		}
 		return false;
@@ -233,24 +223,25 @@ public class State implements GameState {
 
 	@Override
 	public int left() {
-		// TODO Auto-generated method stub
+		// TODO fix only one slide
 		int pointCounter = 0;
 		for (int r = 0; r < 4; r++){
 			for (int c = 3; c > 0; c--) {
-				int curVal = board[r][c];
-				if (board[r][c] == board[r][c-1]) {
+				int curVal = getValue(c, r);
+				int compVal = getValue(c-1, r);
+				if (curVal == compVal) {
 					int newVal = curVal * 2;
 					
-					board[r][c] = 0;//CONSIDER use setValue instead 
-					board[r][c-1] = newVal;
+					setValue(c, r, 0);
+					setValue(c-1, r, newVal);
 					pointCounter = pointCounter + newVal;
 					if (c > 1) {
 						c--;
 					}
 				} else {
-					if (board[r][c-1] == 0) {
-						board[r][c-1] = curVal;
-						board[r][c] = 0;
+					if (compVal == 0) {
+						setValue(c-1, r, curVal);
+						setValue(c, r, 0);
 					}
 				}
 			}
@@ -261,24 +252,25 @@ public class State implements GameState {
 
 	@Override
 	public int right() {
-		// TODO Auto-generated method stub
+		// TODO fix only one slide
 		int pointCounter = 0;
 		for (int r = 0; r <4; r++){
 			for (int c = 0; c < 3; c++) {
-				int curVal = board[r][c];
-				if (board[r][c] == board[r][c+1]) {
+				int curVal = getValue(c, r);
+				int compVal = getValue(c+1, r);
+				if (curVal == compVal) {
 					int newVal = curVal * 2;
 					
-					board[r][c] = 0;//CONSIDER use setValue instead 
-					board[r][c+1] = newVal;
+					setValue(c, r, 0);
+					setValue(c+1, r, newVal);
 					pointCounter = pointCounter + newVal;
 					if (c < 2) {
 						c++;
 					}
 				} else {
-					if (board[r][c+1] == 0) {
-						board[r][c+1] = curVal;
-						board[r][c] = 0;
+					if (compVal == 0) {
+						setValue(c+1, r, curVal);
+						setValue(c, r, 0);
 					}
 				}
 			}
@@ -289,24 +281,25 @@ public class State implements GameState {
 
 	@Override
 	public int down() {
-		// TODO Auto-generated method stub
+		// TODO fix only one slide
 		int pointCounter = 0;
 		for (int c = 0; c <= 3; c++) {
 			for (int r = 0; r < 3; r++) {
-				int curVal = board[r][c];
-				if (board[r][c] == board[r+1][c]) {
+				int curVal = getValue(c, r);
+				int compVal = getValue(c, r+1);;
+				if (curVal == compVal) {
 					int newVal = curVal * 2;
 					
-					board[r][c] = 0;//CONSIDER use setValue instead 
-					board[r+1][c] = newVal;
+					setValue(c, r, 0);
+					setValue(c, r+1, newVal);
 					pointCounter = pointCounter + newVal;
 					if (r < 2) {
 						r++;
 					}
 				} else {
-					if (board[r+1][c] == 0) {
-						board[r+1][c] = curVal;
-						board[r][c] = 0;
+					if (compVal == 0) {
+						setValue(c, r+1, curVal);
+						setValue(c, r, 0);
 					}
 				}
 			}
@@ -317,24 +310,25 @@ public class State implements GameState {
 
 	@Override
 	public int up() {
-		// TODO Auto-generated method stub
+		// TODO fix only one slide, use get + set value
 		int pointCounter = 0;
 		for (int c = 0; c <= 3; c++) {
 			for (int r = 3; r > 0; r--) {
-				int curVal = board[r][c];
-				if (board[r][c] == board[r-1][c]) {
+				int curVal = getValue(c, r);
+				int compVal = getValue(c, r-1);
+				if (curVal == compVal) {
 					int newVal = curVal * 2;
 					
-					board[r][c] = 0;//CONSIDER use setValue instead 
-					board[r-1][c] = newVal;
+					setValue(c, r, 0);
+					setValue(c, r-1, newVal);
 					pointCounter = pointCounter + newVal;
 					if (r > 1) {
 						r--;
 					}
 				} else {
-					if (board[r-1][c] == 0) {
-						board[r-1][c] = curVal;
-						board[r][c] = 0;
+					if (compVal == 0) {
+						setValue(c, r-1, curVal);
+						setValue(c, r, 0);
 					}
 				}
 			}
